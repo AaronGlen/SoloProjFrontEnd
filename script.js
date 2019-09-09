c82e82ac8153fdbf3a81a00ae63cb1025854eebd
AaronGlen <aaglendinning@gmail.com>
2019-09-09 21:49:48 +0100

    input boxes now take in data

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function teamPage(){
 
 location.href = "teams.html";
 }
+const formDataObj = {};
+    
+function handleSubmit(form){
+   
+    for (let element of form.elements) {
+        formDataObj[element.id] = element.value;
+    }
+    console.log(formDataObj);
+    return false;
+}
+function newTableEntries(){
+
+}
