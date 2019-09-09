e8c88805b3644cc7a5ac6ecdf1884168dfb70470
AaronGlen <aaglendinning@gmail.com>
2019-09-09 22:31:01 +0100

    input box's now add entries to table

--- script.js
+++ script.js
@@ -22,8 +22,25 @@ function handleSubmit(form){
         formDataObj[element.id] = element.value;
     }
     console.log(formDataObj);
+    newTableEntries(formDataObj["heroName"],formDataObj["issueOne"],formDataObj["description"]);
     return false;
 }
-function newTableEntries(){
 
+const heroTable = document.getElementById("heroTable");
+
+function newTableEntries(){
+    row = document.createElement("tr")
+    for( let i =0; i <arguments.length;i++){
+        box = document.createElement("td")
+        box.innerHTML = arguments[i];
+        row.append(box)
+    }
+    heroTable.append(row)
+
+}
+
+function newInfo(text, data, section) {
+    test = document.createElement('p');
+    test.innerHTML = text + data;
+    section.append(test);
 }
